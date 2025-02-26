83c087926eb010fc069da2d82f4ae5b590d6fb12
Akshat <www.akshatagarwal123klj@gmail.com>
2025-02-26 12:03:04 +0530

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://server-1-x9s9.onrender.com');
+// const socket = io('https://server-1-x9s9.onrender.com');
+const socket = io('https://server-nw6w.onrender.com'):
 /* To  avoiding the cors origin error refer the local host as below:
   const socket = io('http://localhost:9000');
 */
